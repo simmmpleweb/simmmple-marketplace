9c7c6dc21e0dd19e276b4084f83b8eac7e469dfa
simmmpleweb <vlad@simmmple.com>
2021-10-20 11:23:05 +0300

    Removed errors

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import { Route, Switch } from "react-router-dom";
 import { Box } from "@chakra-ui/react";
 import "assets/css/App.css";
-
 // Custom Components
 import Navbar from "components/navbar/Navbar";
-import MainLayout from "layouts/Main";
+import MainLayout from "layouts/Main.js";
 import Footer from "components/footer/Footer";
 
 function App() {
