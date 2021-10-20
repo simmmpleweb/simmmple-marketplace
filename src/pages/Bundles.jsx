e7bca3be2391491ba6f13dec9b7db5a9b0e649ac
simmmpleweb <vlad@simmmple.com>
2021-10-21 01:27:45 +0300

    Card Pattern

--- src/pages/Bundles.jsx
+++ src/pages/Bundles.jsx
@@ -21,6 +21,16 @@ import {
 
 // Images
 import bundlesBg from "assets/img/bundles/bundles-background.png";
+import medal from "assets/img/bundles/medal.png";
+import trophy from "assets/img/bundles/trophy.png";
+import simmmpleFolder from "assets/img/bundles/simmmple-folder.png";
+import developers from "assets/img/bundles/developers.png";
+import sensualBg from "assets/img/bundles/sensual-background.png";
+import purity from "assets/img/bundles/purity.png";
+import vdb from "assets/img/bundles/vdb.png";
+import sensual from "assets/img/bundles/trophy.png";
+import venus from "assets/img/bundles/venus.png";
+import uranus from "assets/img/bundles/uranus.png";
 
 const Bundles = () => {
   const leftEffect = useRef();
@@ -93,221 +103,304 @@ const Bundles = () => {
           </Flex>
         </Flex>
       </Flex>
-      <Flex bg='#F4F7FE' py='100px'>
-        <Flex direction='column' w='1170px' mx='auto'>
-          <Flex direction='column' align='center' w='100%' mb='72px' mx='auto'>
-            <Text
-              fontSize='xs'
-              color='brand.300'
-              fontWeight='bold'
-              mb='16px'
-              letterSpacing='2px'
-              textAlign='center'>
-              EXCLUSIVE DIGITAL BUNDLE BENEFITS
-            </Text>
-            <Text
-              fontSize='34px'
-              lineHeight='44px'
-              letterSpacing='-1px'
-              color='brand.700'
-              fontWeight='500'
-              mb='20px'
-              mx='auto'
-              textAlign='center'>
-              So... what’s it all about?
-            </Text>
-            <Text
-              fontSize='md'
-              lineHeight='28px'
-              color='#878CBD'
-              fontWeight='400'
-              mx='auto'
-              textAlign='center'>
-              It's about building better products faster than ever before.
-            </Text>
-          </Flex>
-          <Flex>
-            <Card me='20px'>
-              <CardBody px='36px'>
-                <Image />
-              </CardBody>
-            </Card>
-            <Card me='20px'>
-              <CardBody px='36px'>
-                <Image />
-              </CardBody>
-            </Card>
-            <Card me='20px'>
-              <CardBody px='36px'>
-                <Image />
-              </CardBody>
-            </Card>
-            <Card>
-              <CardBody px='36px'>
-                <Image />
-              </CardBody>
-            </Card>
-          </Flex>
-          <ProductList products={featuredProducts} />
+      <Flex py='100px' direction='column' w='1170px' mx='auto'>
+        <Flex direction='column' align='center' w='100%' mb='72px' mx='auto'>
+          <Text
+            fontSize='xs'
+            color='brand.300'
+            fontWeight='bold'
+            mb='16px'
+            letterSpacing='2px'
+            textAlign='center'>
+            EXCLUSIVE DIGITAL BUNDLE BENEFITS
+          </Text>
+          <Text
+            fontSize='34px'
+            lineHeight='44px'
+            letterSpacing='-1px'
+            color='brand.700'
+            fontWeight='500'
+            mb='20px'
+            mx='auto'
+            textAlign='center'>
+            So... what’s it all about?
+          </Text>
+          <Text
+            fontSize='md'
+            lineHeight='28px'
+            color='#878CBD'
+            fontWeight='400'
+            mx='auto'
+            textAlign='center'>
+            It's about building better products faster than ever before.
+          </Text>
+        </Flex>
+        <Flex flexWrap={{ base: "wrap", md: "nowrap" }}>
+          <Card h='320px' me='20px'>
+            <CardBody h='100%' py='12px' justify='space-between' px='20px'>
+              <Flex flexDirection='column' align='center'>
+                <Image src={simmmpleFolder} w='100px' h='100px' mb='auto' />
+                <Text
+                  textAlign='center'
+                  fontWeight='700'
+                  mb='10px'
+                  fontSize='38px'>
+                  4,000+
+                </Text>
+                <Text
+                  textAlign='center'
+                  fontSize='md'
+                  lineHeight='28px'
+                  color='#878CBD'
+                  fontWeight='400'>
+                  Coded & Designed Components and Elements
+                </Text>
+              </Flex>
+            </CardBody>
+          </Card>
+          <Card h='320px' me='20px'>
+            <CardBody h='100%' py='12px' justify='space-between' px='20px'>
+              <Flex flexDirection='column' align='center'>
+                <Flex mb='auto' flexDirection='column'>
+                  <Image src={trophy} w='100px' h='100px' />
+                </Flex>
+                <Text
+                  textAlign='center'
+                  fontWeight='700'
+                  mb='10px'
+                  fontSize='38px'>
+                  4.9/5
+                </Text>
+                <Text
+                  textAlign='center'
+                  fontSize='md'
+                  lineHeight='28px'
+                  color='#878CBD'
+                  fontWeight='400'>
+                  Reviews from developers & designers all over the world
+                </Text>
+              </Flex>
+            </CardBody>
+          </Card>
+          <Card h='320px' me='20px'>
+            <CardBody h='100%' py='12px' justify='space-between' px='20px'>
+              <Flex flexDirection='column' align='center'>
+                <Image src={developers} w='100px' h='100px' mb='auto' />
+                <Text
+                  textAlign='center'
+                  fontWeight='700'
+                  mb='10px'
+                  fontSize='38px'>
+                  10,000+
+                </Text>
+                <Text
+                  textAlign='center'
+                  fontSize='md'
+                  lineHeight='28px'
+                  color='#878CBD'
+                  fontWeight='400'>
+                  Developers & Designers that use our digital products
+                </Text>
+              </Flex>
+            </CardBody>
+          </Card>
+          <Card h='320px'>
+            <CardBody h='100%' py='12px' px='18px'>
+              <Flex flexDirection='column' align='center'>
+                <Image src={medal} w='100px' h='129px' />
+                <Text
+                  mt='auto'
+                  textAlign='center'
+                  fontWeight='700'
+                  mb='10px'
+                  fontSize='38px'>
+                  Lifetime
+                </Text>
+                <Text
+                  textAlign='center'
+                  fontSize='md'
+                  lineHeight='28px'
+                  color='#878CBD'
+                  fontWeight='400'>
+                  We offer Lifetime License for all products of this bundle
+                </Text>
+              </Flex>
+            </CardBody>
+          </Card>
         </Flex>
       </Flex>
-      <Flex direction='column' bg='#fff'>
-        <Flex direction='column' w='1170px' mx='auto' pt='100px' pb='225px'>
+      <Flex mb='100px' direction='column' w='1170px' mx='auto'>
+        <Flex direction='column' align='center' w='100%' mb='72px' mx='auto'>
+          <Text
+            fontSize='xs'
+            color='brand.300'
+            fontWeight='bold'
+            mb='16px'
+            letterSpacing='2px'
+            textAlign='center'>
+            EXCLUSIVE DIGITAL BUNDLE BENEFITS
+          </Text>
+          <Text
+            fontSize='34px'
+            lineHeight='44px'
+            letterSpacing='-1px'
+            color='brand.700'
+            fontWeight='500'
+            mb='20px'
+            mx='auto'
+            textAlign='center'>
+            What Exclusive Digital Bundle contains?
+          </Text>
+          <Text
+            fontSize='md'
+            lineHeight='28px'
+            color='#878CBD'
+            fontWeight='400'
+            mx='auto'
+            textAlign='center'>
+            Well... a lot of cool things, like most of our premium digital
+            products.
+          </Text>
+        </Flex>
+        <Flex flexWrap={{ base: "wrap", md: "nowrap" }}>
           <Flex
-            direction='column'
-            align='center'
-            justify='center'
-            textAlign='center'
-            mb='72px'>
-            <Text fontSize='xs' color='brand.300' fontWeight='bold' mb='16px'>
-              TUTORIALS, ARTICLES & RESOURCES
-            </Text>
-            <Text fontSize='34px' color='brand.700' fontWeight='bold' mb='20px'>
-              Discover resources & guides
-            </Text>
-            <Text
-              fontSize='md'
-              color='#878CBD'
-              fontWeight='normal'
-              maxW='500px'>
-              Discover resources, guides and articles that can help you build
-              unique solutions. Visit our learning centre for heaps of content.
-            </Text>
+            w='100%'
+            h='550px'
+            position='relative'
+            mb='90px'
+            borderRadius='30px'>
+            <Image
+              src={venus}
+              w='575px'
+              h='586px'
+              borderRadius='18px'
+              position='absolute'
+              left='0'
+              top='50%'
+              transform='translate(0px, -50%);'
+              zIndex='2'
+            />
+            <Flex
+              w='100%'
+              h='100%'
+              position='relative'
+              justifyContent='flex-end'
+              overflow='hidden'
+              pe='70px'
+              pt='45px'
+              borderRadius='30px'>
+              <Image
+                position='absolute'
+                transform='translate(-50%, -50%);'
+                w='110%'
+                h='110%'
+                left='53%'
+                top='50%'
+                src={sensualBg}
+                filter='blur(40px)'
+                zIndex='1'
+              />
+              <Flex w='456px' maxW='100%' flexDirection='column' zIndex='2'>
+                <Text fontWeight='bold' fontSize='50px' mb='20px' color='white'>
+                  Venus Design System PRO
+                </Text>
+                <Text
+                  fontSize='md'
+                  lineHeight='28px'
+                  color='white'
+                  w='430px'
+                  maxW='100%'
+                  mb='40px'>
+                  An easy-to-use Design Tool with the most common 2000+ UI/UX
+                  components states that allows you to design quickly with
+                  world-class standards out-of-the-box.
+                </Text>
+                <Flex
+                  w='100%'
+                  justifyContent={{ base: "center", lg: "space-between" }}
+                  align='center'
+                  mb='54px'>
+                  <Flex flexDirection='column'>
+                    <Text
+                      textAlign='center'
+                      fontSize='26px'
+                      lineHeight='33px'
+                      color='white'
+                      fontWeight='700'>
+                      250+
+                    </Text>
+                    <Text
+                      textAlign='center'
+                      fontSize='sm'
+                      lineHeight='14px'
+                      color='white'>
+                      Global Styles
+                    </Text>
+                  </Flex>
+                  <Flex flexDirection='column'>
+                    <Text
+                      textAlign='center'
+                      fontSize='26px'
+                      lineHeight='33px'
+                      color='white'
+                      fontWeight='700'>
+                      2000+
+                    </Text>
+                    <Text
+                      textAlign='center'
+                      fontSize='sm'
+                      lineHeight='14px'
+                      color='white'>
+                      Components States
+                    </Text>
+                  </Flex>
+                  <Flex flexDirection='column'>
+                    <Text
+                      textAlign='center'
+                      fontSize='26px'
+                      lineHeight='33px'
+                      color='white'
+                      fontWeight='700'>
+                      1400+
+                    </Text>
+                    <Text
+                      textAlign='center'
+                      fontSize='sm'
+                      lineHeight='14px'
+                      color='white'>
+                      Material Design Icons
+                    </Text>
+                  </Flex>
+                </Flex>
+                <Flex w='100%' justifyContent='space-between' align='center'>
+                  <Button
+                    color='#020425'
+                    fontWeight='400'
+                    fontSize='sm'
+                    lineHeight='14px'
+                    h='56px'
+                    w='190px'>
+                    Live Preview
+                  </Button>
+                  <Button
+                    color='white'
+                    bg='none'
+                    border='1px solid white'
+                    _hover={{
+                      bg: "none",
+                      opacity: "0.8",
+                    }}
+                    fontWeight='400'
+                    fontSize='sm'
+                    lineHeight='14px'
+                    h='56px'
+                    w='190px'>
+                    More Details
+                  </Button>
+                </Flex>
+              </Flex>
+            </Flex>
           </Flex>
-          <Grid templateColumns='repeat(3, 1fr)' gap='22px'>
-            <NavLink to='/'>
-              <Card
-                boxShadow='45px 76px 113px 7px rgba(112, 144, 176, 0.08)'
-                pb='35px'>
-                <CardBody>
-                  <Flex direction='column'>
-                    <Flex
-                      align='center'
-                      mb='30px'
-                      justify='center'
-                      bg='linear-gradient(135deg, #868CFF 0%, #4318FF 100%)'
-                      borderRadius='24px'
-                      w='100%'
-                      h='230px'>
-                      <ArticleIcon1 w='87' h='105' color='#fff' />
-                    </Flex>
-                    <Tag
-                      color='brand.300'
-                      bg='#ECE9FF'
-                      borderRadius='30px'
-                      p='7px 14px'
-                      maxW='80px'
-                      mb='10px'>
-                      Articles
-                    </Tag>
-                    <Text
-                      color='brand.700'
-                      fontSize='lg'
-                      fontWeight='500'
-                      mb='14px'>
-                      How to build a design system if you're the only designer
-                      in a startup
-                    </Text>
-                    <Text fontSize='sm' color='#878CBD' fontWeight='normal'>
-                      It's always hard to be the only person in a company who is
-                      responsible for product design. Your day...
-                    </Text>
-                  </Flex>
-                </CardBody>
-              </Card>
-            </NavLink>
-            <NavLink to='/'>
-              <Card
-                boxShadow='45px 76px 113px 7px rgba(112, 144, 176, 0.08)'
-                pb='35px'>
-                <CardBody>
-                  <Flex direction='column'>
-                    <Flex
-                      align='center'
-                      direction='column'
-                      mb='30px'
-                      justify='center'
-                      bg='linear-gradient(109.6deg, #FF9966 17.44%, #FF5E62 78.63%)'
-                      borderRadius='24px'
-                      w='100%'
-                      h='230px'>
-                      <ArticleIcon2 w='72px' h='72px' color='#fff' mb='20px' />
-                      <Text color='#fff' fontSize='xl' fontWeight='bold'>
-                        SEO Strategy
-                      </Text>
-                    </Flex>
-                    <Tag
-                      color='brand.300'
-                      bg='#ECE9FF'
-                      borderRadius='30px'
-                      p='7px 14px'
-                      maxW='80px'
-                      mb='10px'>
-                      Articles
-                    </Tag>
-                    <Text
-                      color='brand.700'
-                      fontSize='lg'
-                      fontWeight='500'
-                      mb='14px'>
-                      How to build a design system if you're the only designer
-                      in a startup
-                    </Text>
-                    <Text fontSize='sm' color='#878CBD' fontWeight='normal'>
-                      It's always hard to be the only person in a company who is
-                      responsible for product design. Your day...
-                    </Text>
-                  </Flex>
-                </CardBody>
-              </Card>
-            </NavLink>
-            <NavLink to='/'>
-              <Card
-                boxShadow='45px 76px 113px 7px rgba(112, 144, 176, 0.08)'
-                pb='35px'>
-                <CardBody>
-                  <Flex direction='column'>
-                    <Flex
-                      align='center'
-                      mb='30px'
-                      justify='center'
-                      bg='linear-gradient(90deg, #4481EB 0%, #04BEFE 100%)'
-                      borderRadius='24px'
-                      w='100%'
-                      h='230px'>
-                      <ArticleIcon3 w='87' h='105' color='#fff' />
-                    </Flex>
-                    <Tag
-                      color='brand.300'
-                      bg='#ECE9FF'
-                      borderRadius='30px'
-                      p='7px 14px'
-                      maxW='80px'
-                      mb='10px'>
-                      Articles
-                    </Tag>
-                    <Text
-                      color='brand.700'
-                      fontSize='lg'
-                      fontWeight='500'
-                      mb='14px'>
-                      How to build a design system if you're the only designer
-                      in a startup
-                    </Text>
-                    <Text fontSize='sm' color='#878CBD' fontWeight='normal'>
-                      It's always hard to be the only person in a company who is
-                      responsible for product design. Your day...
-                    </Text>
-                  </Flex>
-                </CardBody>
-              </Card>
-            </NavLink>
-          </Grid>
-          <Button bg='#ECE9FF' w='152px' h='56px' alignSelf='center' mt='52px'>
-            <Text color='brand.300' fontSize='sm'>
-              Learn more
-            </Text>
-          </Button>
         </Flex>
       </Flex>
     </Flex>
