9dd8ba08825f51a7c584f21cd0af0cdd153e3217
simmmpleweb <vlad@simmmple.com>
2021-10-21 09:10:17 +0300

    Added last 3 cards

--- src/pages/Bundles.jsx
+++ src/pages/Bundles.jsx
@@ -28,7 +28,7 @@ import developers from "assets/img/bundles/developers.png";
 import sensualBg from "assets/img/bundles/sensual-background.png";
 import purity from "assets/img/bundles/purity.png";
 import vdb from "assets/img/bundles/vdb.png";
-import sensual from "assets/img/bundles/trophy.png";
+import sensual from "assets/img/bundles/sensual.png";
 import venus from "assets/img/bundles/venus.png";
 import uranus from "assets/img/bundles/uranus.png";
 
@@ -505,6 +505,413 @@ const Bundles = () => {
                 <Flex w='100%' justifyContent='space-between' align='center'>
                   <Button
                     color='#FF4555'
+                    fontWeight='400'
+                    fontSize='sm'
+                    lineHeight='14px'
+                    h='56px'
+                    w='190px'>
+                    Live Preview
+                  </Button>
+                  <Button
+                    color='white'
+                    bg='none'
+                    border='1px solid white'
+                    _hover={{
+                      bg: "none",
+                      opacity: "0.8",
+                    }}
+                    fontWeight='400'
+                    fontSize='sm'
+                    lineHeight='14px'
+                    h='56px'
+                    w='190px'>
+                    More Details
+                  </Button>
+                </Flex>
+              </Flex>
+            </Flex>
+          </Flex>
+        </Flex>
+        <Flex flexWrap={{ base: "wrap", md: "nowrap" }}>
+          <Flex
+            w='100%'
+            h='550px'
+            position='relative'
+            mb='90px'
+            borderRadius='30px'>
+            <Image
+              src={purity}
+              w='575px'
+              h='586px'
+              borderRadius='18px'
+              position='absolute'
+              left='0'
+              top='50%'
+              transform='translate(0px, -50%);'
+              zIndex='2'
+            />
+            <Flex
+              w='100%'
+              h='100%'
+              position='relative'
+              justifyContent='flex-end'
+              overflow='hidden'
+              pe='70px'
+              pt='45px'
+              borderRadius='30px'
+              background='linear-gradient(180deg, #7BCBD4 0%, #29C6B7 100%)'>
+              <Flex w='456px' maxW='100%' flexDirection='column' zIndex='2'>
+                <Text
+                  fontWeight='bold'
+                  fontSize='50px'
+                  mb='20px'
+                  color='white'
+                  letterSpacing='-2.5px'>
+                  Purity UI - Chakra UI Admin Dashboard
+                </Text>
+                <Text
+                  fontSize='md'
+                  lineHeight='28px'
+                  color='white'
+                  w='430px'
+                  maxW='100%'
+                  mb='40px'>
+                  Most trendiest, complex and innovative Dashboard made by
+                  Creative Tim & Simmmple. Check our latest Premium ReactJS
+                  Dashboard based on Chakra UI.
+                </Text>
+                <Flex
+                  w='100%'
+                  justifyContent={{ base: "center", lg: "space-between" }}
+                  align='center'
+                  mb='54px'>
+                  <Flex flexDirection='column'>
+                    <Text
+                      textAlign='center'
+                      fontSize='26px'
+                      lineHeight='33px'
+                      color='white'
+                      fontWeight='700'>
+                      React
+                    </Text>
+                    <Text
+                      textAlign='center'
+                      fontSize='sm'
+                      lineHeight='14px'
+                      color='white'>
+                      Technology
+                    </Text>
+                  </Flex>
+                  <Flex flexDirection='column'>
+                    <Text
+                      textAlign='center'
+                      fontSize='26px'
+                      lineHeight='33px'
+                      color='white'
+                      fontWeight='700'>
+                      300+
+                    </Text>
+                    <Text
+                      textAlign='center'
+                      fontSize='sm'
+                      lineHeight='14px'
+                      color='white'>
+                      Individual Elements
+                    </Text>
+                  </Flex>
+                  <Flex flexDirection='column'>
+                    <Text
+                      textAlign='center'
+                      fontSize='26px'
+                      lineHeight='33px'
+                      color='white'
+                      fontWeight='700'>
+                      Full
+                    </Text>
+                    <Text
+                      textAlign='center'
+                      fontSize='sm'
+                      lineHeight='14px'
+                      color='white'>
+                      Documentation
+                    </Text>
+                  </Flex>
+                </Flex>
+                <Flex w='100%' justifyContent='space-between' align='center'>
+                  <Button
+                    color='#30C7BA'
+                    fontWeight='400'
+                    fontSize='sm'
+                    lineHeight='14px'
+                    h='56px'
+                    w='190px'>
+                    Live Preview
+                  </Button>
+                  <Button
+                    color='white'
+                    bg='none'
+                    border='1px solid white'
+                    _hover={{
+                      bg: "none",
+                      opacity: "0.8",
+                    }}
+                    fontWeight='400'
+                    fontSize='sm'
+                    lineHeight='14px'
+                    h='56px'
+                    w='190px'>
+                    More Details
+                  </Button>
+                </Flex>
+              </Flex>
+            </Flex>
+          </Flex>
+        </Flex>
+        <Flex flexWrap={{ base: "wrap", md: "nowrap" }}>
+          <Flex
+            w='100%'
+            h='550px'
+            position='relative'
+            mb='90px'
+            borderRadius='30px'>
+            <Image
+              src={uranus}
+              w='575px'
+              h='586px'
+              borderRadius='18px'
+              position='absolute'
+              right='0'
+              top='50%'
+              transform='translate(5%, -50%);'
+              zIndex='2'
+            />
+            <Flex
+              w='100%'
+              h='100%'
+              position='relative'
+              justifyContent='flex-start'
+              overflow='hidden'
+              ps='70px'
+              pt='45px'
+              borderRadius='30px'
+              background='linear-gradient(135deg, #868CFF 0%, #4318FF 100%)'>
+              <Flex w='456px' maxW='100%' flexDirection='column' zIndex='2'>
+                <Text fontWeight='bold' fontSize='50px' mb='20px' color='white'>
+                  Uranus Bootstrap Wireframe Kit
+                </Text>
+                <Text
+                  fontSize='md'
+                  lineHeight='28px'
+                  color='white'
+                  w='460px'
+                  maxW='100%'
+                  mb='40px'
+                  letterSpacing='-0.8px'>
+                  Uranus - Wireframe Kit is a wireframing pack with more than
+                  180+ dark/light ready-to-use web elements/sections. The
+                  basement idea of this kit was started from the wireframing
+                  time problem.
+                </Text>
+                <Flex
+                  w='100%'
+                  justifyContent={{ base: "center", lg: "space-between" }}
+                  align='center'
+                  mb='54px'>
+                  <Flex flexDirection='column'>
+                    <Text
+                      textAlign='center'
+                      fontSize='26px'
+                      lineHeight='33px'
+                      color='white'
+                      fontWeight='700'>
+                      Bootstrap
+                    </Text>
+                    <Text
+                      textAlign='center'
+                      fontSize='sm'
+                      lineHeight='14px'
+                      color='white'>
+                      Wireframe Kit
+                    </Text>
+                  </Flex>
+                  <Flex flexDirection='column'>
+                    <Text
+                      textAlign='center'
+                      fontSize='26px'
+                      lineHeight='33px'
+                      color='white'
+                      fontWeight='700'>
+                      180+
+                    </Text>
+                    <Text
+                      textAlign='center'
+                      fontSize='sm'
+                      lineHeight='14px'
+                      color='white'>
+                      Widgets/Cards
+                    </Text>
+                  </Flex>
+                  <Flex flexDirection='column'>
+                    <Text
+                      textAlign='center'
+                      fontSize='26px'
+                      lineHeight='33px'
+                      color='white'
+                      fontWeight='700'>
+                      Dark/Light
+                    </Text>
+                    <Text
+                      textAlign='center'
+                      fontSize='sm'
+                      lineHeight='14px'
+                      color='white'>
+                      Themes
+                    </Text>
+                  </Flex>
+                </Flex>
+                <Flex w='100%' justifyContent='space-between' align='center'>
+                  <Button
+                    color='#634FFF'
+                    fontWeight='400'
+                    fontSize='sm'
+                    lineHeight='14px'
+                    h='56px'
+                    w='190px'>
+                    Live Preview
+                  </Button>
+                  <Button
+                    color='white'
+                    bg='none'
+                    border='1px solid white'
+                    _hover={{
+                      bg: "none",
+                      opacity: "0.8",
+                    }}
+                    fontWeight='400'
+                    fontSize='sm'
+                    lineHeight='14px'
+                    h='56px'
+                    w='190px'>
+                    More Details
+                  </Button>
+                </Flex>
+              </Flex>
+            </Flex>
+          </Flex>
+        </Flex>
+        <Flex flexWrap={{ base: "wrap", md: "nowrap" }}>
+          <Flex
+            w='100%'
+            h='550px'
+            position='relative'
+            mb='90px'
+            borderRadius='30px'>
+            <Image
+              src={sensual}
+              w='575px'
+              h='586px'
+              borderRadius='18px'
+              position='absolute'
+              left='0'
+              top='50%'
+              transform='translate(0px, -50%);'
+              zIndex='2'
+            />
+            <Flex
+              w='100%'
+              h='100%'
+              position='relative'
+              justifyContent='flex-end'
+              overflow='hidden'
+              pe='70px'
+              pt='45px'
+              borderRadius='30px'>
+              <Image
+                position='absolute'
+                transform='translate(-50%, -50%);'
+                w='110%'
+                h='110%'
+                left='53%'
+                top='50%'
+                src={sensualBg}
+                filter='blur(40px)'
+                zIndex='1'
+              />
+              <Flex w='456px' maxW='100%' flexDirection='column' zIndex='2'>
+                <Text fontWeight='bold' fontSize='50px' mb='20px' color='white'>
+                  Purity UI React Sensual Kit
+                </Text>
+                <Text
+                  fontSize='md'
+                  lineHeight='28px'
+                  color='white'
+                  w='430px'
+                  maxW='100%'
+                  mb='40px'>
+                  Most trendiest, complex and innovative Dashboard made by
+                  Simmmple. Check our latest Premium ReactJS Dashboard based on
+                  Chakra UI.
+                </Text>
+                <Flex
+                  w='100%'
+                  justifyContent={{ base: "center", lg: "space-between" }}
+                  align='center'
+                  mb='54px'>
+                  <Flex flexDirection='column'>
+                    <Text
+                      textAlign='center'
+                      fontSize='26px'
+                      lineHeight='33px'
+                      color='white'
+                      fontWeight='700'>
+                      Trendy
+                    </Text>
+                    <Text
+                      textAlign='center'
+                      fontSize='sm'
+                      lineHeight='14px'
+                      color='white'>
+                      Design Styles
+                    </Text>
+                  </Flex>
+                  <Flex flexDirection='column'>
+                    <Text
+                      textAlign='center'
+                      fontSize='26px'
+                      lineHeight='33px'
+                      color='white'
+                      fontWeight='700'>
+                      300+
+                    </Text>
+                    <Text
+                      textAlign='center'
+                      fontSize='sm'
+                      lineHeight='14px'
+                      color='white'>
+                      Individual Components
+                    </Text>
+                  </Flex>
+                  <Flex flexDirection='column'>
+                    <Text
+                      textAlign='center'
+                      fontSize='26px'
+                      lineHeight='33px'
+                      color='white'
+                      fontWeight='700'>
+                      Full
+                    </Text>
+                    <Text
+                      textAlign='center'
+                      fontSize='sm'
+                      lineHeight='14px'
+                      color='white'>
+                      Documentation
+                    </Text>
+                  </Flex>
+                </Flex>
+                <Flex w='100%' justifyContent='space-between' align='center'>
+                  <Button
+                    color='#020425'
                     fontWeight='400'
                     fontSize='sm'
                     lineHeight='14px'
