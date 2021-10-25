9175c10735f691aa06f7ffa4656ba20838f0c5ad
simmmpleweb <vlad@simmmple.com>
2021-10-25 15:45:43 +0300

    License page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Box } from "@chakra-ui/react";
+import { Box, useColorMode, Button } from "@chakra-ui/react";
 import "assets/css/App.css";
 // Custom Components
 import Navbar from "components/navbar/Navbar";
@@ -6,8 +6,12 @@ import MainLayout from "layouts/Main.js";
 import Footer from "components/footer/Footer";
 
 function App() {
+  const { colorMode, toggleColorMode } = useColorMode();
   return (
     <Box>
+      <Button onClick={toggleColorMode}>
+        Toggle {colorMode === "light" ? "Dark" : "Light"}
+      </Button>
       <Navbar />
       <MainLayout />
       <Footer />
