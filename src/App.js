c4e0418e083915a4b5d31620120fffb8af615e18
simmmpleweb <vlad@simmmple.com>
2021-10-27 02:45:19 +0300

    Removed helpers

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Box, useColorMode, Button } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
 import "assets/css/App.css";
 // Custom Components
 import Navbar from "components/navbar/Navbar";
@@ -6,12 +6,8 @@ import MainLayout from "layouts/Main.js";
 import Footer from "components/footer/Footer";
 
 function App() {
-  const { colorMode, toggleColorMode } = useColorMode();
   return (
     <Box>
-      <Button onClick={toggleColorMode}>
-        Toggle {colorMode === "light" ? "Dark" : "Light"}
-      </Button>
       <Navbar />
       <MainLayout />
       <Footer />
