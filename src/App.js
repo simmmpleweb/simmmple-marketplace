5bb2b186562006a31acbfd95395d1d54dc0d44f5
simmmpleweb <vlad@simmmple.com>
2021-10-31 17:16:39 +0200

    Bundles Responsive

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Box } from "@chakra-ui/react";
 import "assets/css/App.css";
 // Custom Components
 import Navbar from "components/navbar/Navbar";
-import MainLayout from "layouts/main.js";
+import MainLayout from "layouts/Main.js";
 import Footer from "components/footer/Footer";
 
 function App() {
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -61,7 +61,7 @@ const Home = () => {
           minH={{ base: "800px", md: "870px" }}
           bgImage={homeBg}
           bgSize='cover'
-          bgPosition={{ base: "-60px 0px", md: "center" }}
+          bgPosition={{ base: "-46px 0px", md: "center" }}
           position='relative'>
           <Box
             position='absolute'
